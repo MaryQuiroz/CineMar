3276d18b8eac23efe1bdf1c12f63bd4737199d1a
Mary Quiroz <maryqr21@gmail.com>
2024-11-25 18:40:31 +0100

    again

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/cinemar/',
+  base: '/CineMar/',
   plugins: [react()],
   server: {
     historyApiFallback: {
